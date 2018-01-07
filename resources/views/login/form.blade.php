5122db8a8eb0d289653f592224af462d9c1331e2
dennismiano <dennismiano64@gmail.com>
2018-01-07 04:04:24 -0800

    Final rounds of cleanup

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -3,22 +3,39 @@
 	<head>
 	<script src="{{ URL::asset('/js/jquery-3.2.1.min.js')}}"></script>
      <script src="{{ URL::asset('/js/login/login.js')}}"></script>
+     <link href="{{ URL::asset('/css/bootstrap.min.css')}}" rel="stylesheet">
+     <link href="{{ URL::asset('/css/spess.css')}}" rel="stylesheet">
 	 <title>Admin</title>
 
 	</head>
 
 	<body>
 
-			<div>
-			
-			   <form class="login_form">
-			   {{csrf_field()}}
-				  <input type="email"  placeholder="Enter Email" name="email" />
-				  <input type="password"  placeholder="Enter Password"  name="pass"/>
-			   
-				   <button type="submit" >Log In</button>
-			   </form>
-			</div>
+				<div class="col-md-4 col-md-offset-4">
+					<div class="login-form">
+						<div class="login-form-header">
+							<h1>SPESS Admin Login</h1>
+						</div>
+
+						<div class="login-form-body">
+							   <form class="login_form">
+							   {{csrf_field()}}
+
+								   	<div class="form-group">
+									  <input type="email" class="form-control"  placeholder="Enter Email" name="email" />
+									</div>
+
+									<div class="form-group">
+									  <input type="password" class="form-control"  placeholder="Enter Password"  name="pass"/>
+								   	</div>
+
+								   	<div class="form-group">
+									   <button type="submit" class="btn btn-warning" >Log In</button>
+									</div>
+							   </form>
+						</div>
+					</div>
+				</div>
 
 	</body>
 
